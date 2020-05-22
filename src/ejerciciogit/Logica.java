f871bb6c068c932278d75fb09b56005d3008fc23
GabrielChuchuca <gavriellalx@hotmail.com>
2020-05-21 19:43:24 -0500

    Subida #1

---
+++ src/ejerciciogit/Logica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author Gabriel Leonardo Chu
+ */
+public class Logica {
+    public String metodoIntegrante1(){
+        return "integrante 1";
+    }
+    public String metodoIntegrante2(){
+        return "integrante 2";
+    }
+    public String metodoIntegrante3(){
+        return "integrante 3";
+    }
+    public String metodoIntegrante4(){
+        return "integrante 4";
+    }
+}
---
+++ src/ejerciciogit/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author Gabriel Leonardo Chu
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Logica logica =new Logica(); 
+        System.out.println(logica.metodoIntegrante1());//Gabriel Chuchuca
+        System.out.println(logica.metodoIntegrante2());//Andres Loja
+        System.out.println(logica.metodoIntegrante3());//Xavier Jarro
+        System.out.println(logica.metodoIntegrante4());//Vinicio Veletanga
+    }   
+}
